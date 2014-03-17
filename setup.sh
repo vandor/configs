d2f0020dd88efae9bd6c9226d21ec8e63e39aad8
Josh Loya <josh@Vim.local>
2014-03-16 22:23:58 -0500

    Improve install script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+#TODO: Make this script idempotent
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 _linkfiles () {
 	# Get filenames
 	local_path="$DIR/$1"
-	echo $local_path
 	home_path="$HOME/${2:-$1}"
-	echo $home_path
 
 	# Backup existing file
 	if [ -e $home_path ]; then
@@ -27,9 +27,11 @@ _linkfiles () {
 _linkfiles .bash_aliases
 _linkfiles .git_aliases
 _linkfiles .git_bash_prompt
+_linkfiles .gitconfig
 _linkfiles solarized_dircolors.ansi-dark .dircolors
 _linkfiles .tmux.conf
 
+# TODO: Make these idempotent
 cat $DIR/.bash_profile_additions >> $HOME/.bash_profile
 cat $DIR/.bashrc_additions >> $HOME/.bashrc
 
